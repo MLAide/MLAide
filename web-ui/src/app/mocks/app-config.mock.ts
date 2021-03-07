1733f115a147782cf9f4d377fb00d737c10bd8ca
Farruch Kouliev <farruch.kouliev@gmail.com>
2021-03-07 15:38:51 +0100

    MOD-47 Fixed app config mock

--- web-ui/src/app/mocks/app-config.mock.ts
+++ web-ui/src/app/mocks/app-config.mock.ts
@@ -10,6 +10,7 @@ export const appConfigMock: IAppConfig = {
   },
   auth: {
     allowedUrls: "http://localhost:9000",
+    audience: "https://api.mvc.io",
     clientId: "27Z4KgE8vTuMo4VH0874o5C42M5scmNv",
     customQueryParams: {
       audience: "https://api.mvc.io",
