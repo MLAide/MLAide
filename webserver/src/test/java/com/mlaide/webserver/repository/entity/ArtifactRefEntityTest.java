d88ec96d3c4638bd2558eb3cb85d7af3e6c610c5
Farruch Kouliev <farruch.kouliev@gmail.com>
2021-04-01 06:25:28 +0200

    MOD-139 Added Validation and Tests for ArtifactRefEntity

---
+++ webserver/src/test/java/com/mlaide/webserver/repository/entity/ArtifactRefEntityTest.java
@@ -0,0 +1,81 @@
+package com.mlaide.webserver.repository.entity;
+
+import com.mlaide.webserver.configuration.MongoConfig;
+import com.mlaide.webserver.integration.MongoDB;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import javax.validation.ConstraintViolationException;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@DataMongoTest
+@ExtendWith(SpringExtension.class)
+@Import(MongoConfig.class)
+@Testcontainers
+public class ArtifactRefEntityTest {
+    @Autowired
+    private MongoTemplate mongo;
+    @Container
+    private static final MongoDB mongoDB = new MongoDB();
+    @DynamicPropertySource
+    public static void mongoDBProperties(DynamicPropertyRegistry registry) {
+        mongoDB.updateSpringProperties(registry);
+    }
+    private ArtifactRefEntity artifactRefEntity;
+
+    @TestConfiguration
+    static class MongoMapKeyDotReplacementConfiguration {
+        @Bean
+        public LocalValidatorFactoryBean localValidatorFactoryBean() {
+            return new LocalValidatorFactoryBean();
+        }
+    }
+
+    @BeforeEach
+    void initialize(){
+        artifactRefEntity = new ArtifactRefEntity();
+        artifactRefEntity.setName("artifactRefEntity-test");
+        artifactRefEntity.setVersion(1);
+    }
+
+    @Nested
+    class validation {
+        @ParameterizedTest
+        @NullSource
+        @ValueSource(strings = {"", " "})
+        void invalid_name_should_throw_ConstraintViolationException(String arg) {
+            // Arrange
+            artifactRefEntity.setName(arg);
+
+            // Act + Assert
+            assertThatThrownBy(() -> mongo.save(artifactRefEntity)).isInstanceOf(ConstraintViolationException.class);
+        }
+
+        @Test
+        void version_is_null_should_throw_ConstraintViolationException() {
+            // Arrange
+            artifactRefEntity.setVersion(null);
+
+            // Act + Assert
+            assertThatThrownBy(() -> mongo.save(artifactRefEntity)).isInstanceOf(ConstraintViolationException.class);
+        }
+    }
+}
