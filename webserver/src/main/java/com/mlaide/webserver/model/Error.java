10a03859912a0f3097415dd111251d4b6fbd1542
Raman <mail.raman.s@gmail.com>
2021-04-11 22:01:42 +0200

    Handle bad requests without error message (#16)

--- webserver/src/main/java/com/mlaide/webserver/model/Error.java
+++ webserver/src/main/java/com/mlaide/webserver/model/Error.java
@@ -1,10 +1,8 @@
 package com.mlaide.webserver.model;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import com.mlaide.webserver.validation.Violation;
 import lombok.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Getter
@@ -14,7 +12,11 @@ import java.util.List;
 public class Error {
     @NonNull
     private int code;
-    @NonNull
     private String message;
     private List<Violation> violations;
+
+    public Error(int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
 }
