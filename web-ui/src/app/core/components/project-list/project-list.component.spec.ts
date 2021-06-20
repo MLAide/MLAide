509b5cba8084ef973cf830c9bcb3996c567b8f75
Raman <mail.raman.s@gmail.com>
2021-06-20 19:10:03 +0200

    Add unit test for project-list component

--- web-ui/src/app/core/components/project-list/project-list.component.spec.ts
+++ web-ui/src/app/core/components/project-list/project-list.component.spec.ts
@@ -16,7 +16,8 @@ import { MockPipe } from "ng-mocks";
 import { MatSnackBarModule } from "@angular/material/snack-bar";
 import { provideMockStore, MockStore } from '@ngrx/store/testing';
 import { AppState } from "@mlaide/state/app.state";
-import { openCreateProjectDialog } from "@mlaide/state/project/project.actions";
+import { loadProjects, openCreateProjectDialog } from "@mlaide/state/project/project.actions";
+import { Action } from "@ngrx/store";
 
 describe("ProjectListComponent", () => {
   let component: ProjectListComponent;
@@ -25,6 +26,7 @@ describe("ProjectListComponent", () => {
 
   const routerSpy = jasmine.createSpyObj("Router", ["navigateByUrl"]);
   let store: MockStore;
+  let dispatchSpy: jasmine.Spy<(action: Action) => void>;
 
   beforeEach(async () => {
     const initialState = createAppState([]);
@@ -39,6 +41,7 @@ describe("ProjectListComponent", () => {
     }).compileComponents();
 
     store = TestBed.inject(MockStore);
+    dispatchSpy = spyOn(store, 'dispatch');
   });
 
   beforeEach(() => {
@@ -61,91 +64,99 @@ describe("ProjectListComponent", () => {
       // assert
       expect(h1.textContent).toEqual("Projects Overview");
     });
+  });
 
-    describe("add projects", () => {
-      const addProjectButtonTitle = "Add Project";
-      it("should contain add project button", () => {
-        // arrange
-        let addProjectsButton: HTMLElement = fixture.nativeElement.querySelector("button");
+  describe("add projects", () => {
+    const addProjectButtonTitle = "Add Project";
+    it("should contain add project button", () => {
+      // arrange
+      let addProjectsButton: HTMLElement = fixture.nativeElement.querySelector("button");
 
-        // assert
-        expect(addProjectsButton.textContent).toBeTruthy();
-        expect(addProjectsButton.textContent).toContain(addProjectButtonTitle);
-      });
-
-      it("should dispatch openCreateProjectDialog action on clicking the add project button", async () => {
-        // arrange
-        const dispatchSpy = spyOn(store, 'dispatch');
-        const addProjectButton = await loader.getHarness(MatButtonHarness.with({ text: addProjectButtonTitle }));
-
-        // act
-        await addProjectButton.click();
-
-        // assert
-        expect(dispatchSpy).toHaveBeenCalledWith(openCreateProjectDialog());
-      });
+      // assert
+      expect(addProjectsButton.textContent).toBeTruthy();
+      expect(addProjectsButton.textContent).toContain(addProjectButtonTitle);
     });
 
-    describe("projects table", () => {
-      it("should contain the projects table", () => {
-        // arrange
-        let projectTable: HTMLElement = fixture.nativeElement.querySelector("table");
+    it("should dispatch openCreateProjectDialog action on clicking the add project button", async () => {
+      // arrange
+      const addProjectButton = await loader.getHarness(MatButtonHarness.with({ text: addProjectButtonTitle }));
 
-        // assert
-        expect(projectTable.textContent).toBeTruthy();
-      });
+      // act
+      await addProjectButton.click();
 
-      it("should have defined headers", async () => {
-        // arrange
-        const table: MatTableHarness = await loader.getHarness(MatTableHarness);
-        const headers: MatHeaderRowHarness[] = await table.getHeaderRows();
-        const headerRow: MatRowHarnessColumnsText = await headers[0].getCellTextByColumnName();
+      // assert
+      expect(dispatchSpy).toHaveBeenCalledWith(openCreateProjectDialog());
+    });
+  });
 
-        // assert
-        expect(Object.keys(headerRow).length).toBe(3);
-        expect(headerRow.name).toBe("Name");
-        expect(headerRow.key).toBe("Key");
-        expect(headerRow.createdAt).toBe("Created at");
-      });
+  describe("projects table", () => {
+    it("should contain the projects table", () => {
+      // arrange
+      let projectTable: HTMLElement = fixture.nativeElement.querySelector("table");
 
-      it("should show row for each project", async () => {
-        // arrange
-        const projects = await getRandomProjects(3);
-        store.setState(createAppState(projects));
+      // assert
+      expect(projectTable.textContent).toBeTruthy();
+    });
 
-        // act
-        fixture.detectChanges();
+    it("should have defined headers", async () => {
+      // arrange
+      const table: MatTableHarness = await loader.getHarness(MatTableHarness);
+      const headers: MatHeaderRowHarness[] = await table.getHeaderRows();
+      const headerRow: MatRowHarnessColumnsText = await headers[0].getCellTextByColumnName();
 
-        // assert
-        const table: MatTableHarness = await loader.getHarness(MatTableHarness);
-        const rows: MatRowHarness[] = await table.getRows();
-        expect(rows.length).toBe(projects.length);
-        for (let index = 0; index < projects.length; index++) {
-          const project = projects[index];
-          
-          const row: MatRowHarnessColumnsText = await rows[index].getCellTextByColumnName();
+      // assert
+      expect(Object.keys(headerRow).length).toBe(3);
+      expect(headerRow.name).toBe("Name");
+      expect(headerRow.key).toBe("Key");
+      expect(headerRow.createdAt).toBe("Created at");
+    });
 
-          expect(row.name).toEqual(project.name);
-          expect(row.key).toEqual(project.key);
-          expect(row.createdAt).toEqual(String(project.createdAt));
-        }
-      });
+    it("should show row for each project", async () => {
+      // arrange
+      const projects = await getRandomProjects(3);
+      store.setState(createAppState(projects));
 
-      it("should navigate to project experiment page on clicking the project button", async () => {
-        // arrange data for projects table
-        const projects = [{ key: "my-project", name: "My Project", createdAt: new Date() }];
-        store.setState(createAppState(projects));
+      // act
+      fixture.detectChanges();
 
-        // act
-        fixture.detectChanges();
-        const projectButton = await loader.getHarness(MatButtonHarness.with({ text: projects[0].name }));
-        await projectButton.click();
+      // assert
+      const table: MatTableHarness = await loader.getHarness(MatTableHarness);
+      const rows: MatRowHarness[] = await table.getRows();
+      expect(rows.length).toBe(projects.length);
+      for (let index = 0; index < projects.length; index++) {
+        const project = projects[index];
+        
+        const row: MatRowHarnessColumnsText = await rows[index].getCellTextByColumnName();
 
-        // assert
-        const spy = routerSpy.navigateByUrl as jasmine.Spy;
-        expect(spy.calls.count()).toBe(1, "expected navigation router to be called once");
-        expect(spy.calls.first().args[0]).toBe("/projects/my-project");
-      });
+        expect(row.name).toEqual(project.name);
+        expect(row.key).toEqual(project.key);
+        expect(row.createdAt).toEqual(String(project.createdAt));
+      }
+    });
+
+    it("should navigate to project experiment page on clicking the project button", async () => {
+      // arrange data for projects table
+      const projects = [{ key: "my-project", name: "My Project", createdAt: new Date() }];
+      store.setState(createAppState(projects));
+
+      // act
+      fixture.detectChanges();
+      const projectButton = await loader.getHarness(MatButtonHarness.with({ text: projects[0].name }));
+      await projectButton.click();
+
+      // assert
+      const spy = routerSpy.navigateByUrl as jasmine.Spy;
+      expect(spy.calls.count()).toBe(1, "expected navigation router to be called once");
+      expect(spy.calls.first().args[0]).toBe("/projects/my-project");
+    });
+  });
+
+  describe("ngOnInit", () => {
+    it("should dispatch loadProjects action", () => {
+      // ngOnInit will be called in beforeEach while creating the component
+
+      // assert
+      expect(dispatchSpy).toHaveBeenCalledWith(loadProjects());
     });
   });
 
