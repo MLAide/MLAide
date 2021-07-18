3bd91ab2972649d9dc884b9933415b1b1c0b2de6
Farruch Kouliev <farruch.kouliev@gmail.com>
2021-07-18 15:38:33 +0200

    MOD-117 Added unit tests for run.selectors.ts

--- web-ui/src/app/state/run/run.selectors.spec.ts
+++ web-ui/src/app/state/run/run.selectors.spec.ts
@@ -1,9 +1,50 @@
 import { AppState } from "@mlaide/state/app.state";
 import { getRandomRuns } from "@mlaide/mocks/fake-generator";
-import { selectRunsOfCurrentExperiment, selectIsLoadingRuns } from "@mlaide/state/run/run.selectors";
+import {
+  selectRunsOfCurrentExperiment,
+  selectIsLoadingRuns,
+  selectRuns,
+  selectSelectedRunKeys
+} from "@mlaide/state/run/run.selectors";
 import { RunState } from "@mlaide/state/run/run.state";
 
 describe("RunSelectors", () => {
+  describe("selectIsLoadingRuns", () => {
+    it("should select isLoading from state", async () => {
+      // arrange
+      const partialRunState: Partial<RunState> = {
+        isLoading: true
+      };
+      const state: Partial<AppState> = {
+        runs: partialRunState as RunState
+      };
+
+      // act
+      const isLoading = selectIsLoadingRuns(state as AppState);
+
+      // assert
+      expect(isLoading).toBeTrue();
+    });
+  });
+
+  describe("selectRuns", () => {
+    it("should select runs for current experiment from state", async () => {
+      // arrange
+      const partialRunState: Partial<RunState> = {
+        items: await getRandomRuns(3)
+      };
+      const state: Partial<AppState> = {
+        runs: partialRunState as RunState
+      };
+
+      // act
+      const runs = selectRuns(state as AppState);
+
+      // assert
+      expect(runs).toBe(state.runs.items);
+    });
+  });
+
   describe("selectRunsOfCurrentExperiment", () => {
     it("should select runs for current experiment from state", async () => {
       // arrange
@@ -22,21 +63,27 @@ describe("RunSelectors", () => {
     });
   });
 
-  describe("selectIsLoadingRuns", () => {
-    it("should select isLoading from state", async () => {
+  describe("selectSelectedRunKeys", () => {
+    it("should select current run keys from query params of router state", async () => {
       // arrange
-      const partialRunState: Partial<RunState> = {
-        isLoading: true
-      };
-      const state: Partial<AppState> = {
-        runs: partialRunState as RunState
+      const state = {
+        router: {
+          state: {
+            url: "",
+            root: {
+              queryParams: {
+                runKeys: [1,2]
+              }
+            }
+          }
+        }
       };
 
       // act
-      const isLoading = selectIsLoadingRuns(state as AppState);
+      const runKeys = selectSelectedRunKeys(state as any);
 
       // assert
-      expect(isLoading).toBeTrue();
+      expect(runKeys).toEqual([1,2]);
     });
   });
 });
--- web-ui/src/app/state/run/run.selectors.ts
+++ web-ui/src/app/state/run/run.selectors.ts
@@ -3,9 +3,9 @@ import { AppState } from "../app.state";
 import { selectQueryParam } from "../router.selectors";
 
 
-export const selectRunsOfCurrentExperiment = (state: AppState) => state.runs.runsOfCurrentExperiment;
 export const selectIsLoadingRuns = (state: AppState) => state.runs.isLoading;
 export const selectRuns = (state: AppState) => state.runs.items;
+export const selectRunsOfCurrentExperiment = (state: AppState) => state.runs.runsOfCurrentExperiment;
 
 // override the response type because runKeys query param will always be an array of strings
 export const selectSelectedRunKeys: MemoizedSelector<object, number[], DefaultProjectorFn<string>> = selectQueryParam("runKeys") as any;
