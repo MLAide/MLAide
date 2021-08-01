7a538d2847c98a5be395a51f4fd77951cca8922e
Farruch Kouliev <farruch.kouliev@gmail.com>
2021-08-01 14:34:37 +0200

    MOD-117 updated unit tests for experiment-details.component.spec.ts

--- web-ui/src/app/experiments/experiment-details/experiment-details.component.spec.ts
+++ web-ui/src/app/experiments/experiment-details/experiment-details.component.spec.ts
@@ -1,80 +1,86 @@
-/*import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { MatButtonModule } from "@angular/material/button";
 import { MatDialogModule } from "@angular/material/dialog";
 import { MatTableModule } from "@angular/material/table";
 import { MatCardModule } from "@angular/material/card";
-import { ActivatedRoute, ParamMap } from "@angular/router";
-import { Observable, of, Subscription } from "rxjs";
-import { Artifact, ArtifactListResponse } from "@mlaide/entities/artifact.model";
+import { Artifact } from "@mlaide/entities/artifact.model";
 import { Experiment } from "@mlaide/entities/experiment.model";
-import { getRandomExperiment, getRandomProject, getRandomRuns } from "src/app/mocks/fake-generator";
-import { Run, RunListResponse } from "@mlaide/entities/run.model";
-import { GraphEdge, GraphNode, LineageGraphUiService } from "@mlaide/shared/services";
-import { ListDataSourceMock } from "src/app/mocks/data-source.mock";
-import { MockComponent } from "ng-mocks";
-import { MatCardHarness } from "@angular/material/card/testing";
+import { getRandomArtifacts, getRandomExperiment, getRandomProject, getRandomRuns } from "src/app/mocks/fake-generator";
+import { Run } from "@mlaide/entities/run.model";
+import { LineageGraphUiService } from "@mlaide/shared/services";
+import { MockComponent, ngMocks } from "ng-mocks";
 
 import { ExperimentDetailsComponent } from "./experiment-details.component";
 import { Project } from "@mlaide/entities/project.model";
-import { HarnessLoader } from "@angular/cdk/testing";
-import { TestbedHarnessEnvironment } from "@angular/cdk/testing/testbed";
 import { ArtifactsListTableComponent } from "src/app/shared/components/artifacts-list-table/artifacts-list-table.component";
 import { RunsListTableComponent } from "src/app/shared/components/runs-list-table/runs-list-table.component";
-import { ArtifactsApiService, ExperimentsApiService, RunsApiService } from "@mlaide/shared/api";
+import { MockStore, provideMockStore } from "@ngrx/store/testing";
+import { Action } from "@ngrx/store";
+import {
+  selectArtifactsByRunKeys,
+  selectIsLoadingArtifacts
+} from "@mlaide/state/artifact/artifact.selectors";
+import { selectCurrentProjectKey } from "@mlaide/state/project/project.selectors";
+import { selectCurrentExperiment } from "@mlaide/state/experiment/experiment.selectors";
+import { selectIsLoadingRuns, selectRunsOfCurrentExperiment } from "@mlaide/state/run/run.selectors";
+import { loadExperimentWithAllDetails } from "@mlaide/state/experiment/experiment.actions";
+import { ExperimentLineageVisualizationComponent } from "@mlaide/experiments/experiment-lineage-visualization/experiment-lineage-visualization.component";
 
 describe("ExperimentDetailsComponent", () => {
   let fixture: ComponentFixture<ExperimentDetailsComponent>;
   let component: ExperimentDetailsComponent;
 
   // fakes
+  let fakeArtifacts: Artifact[];
   let fakeExperiment: Experiment;
   let fakeProject: Project;
-
-  // route spy
-  let paramMapObservable: Observable<ParamMap>;
-  let unsubscriptionSpy: jasmine.Spy<() => void>;
+  let fakeRuns: Run[];
 
   // service stubs
-  let artifactsApiServiceStub: jasmine.SpyObj<ArtifactsApiService>;
-  let experimentsApiServiceStub: jasmine.SpyObj<ExperimentsApiService>;
   let lineageGraphServiceStub: jasmine.SpyObj<LineageGraphUiService>;
-  let runsApiServiceStub: jasmine.SpyObj<RunsApiService>;
 
-  // data source mocks
-  let artifactListDataSourceMock: ListDataSourceMock<Artifact, ArtifactListResponse> = new ListDataSourceMock();
-  let runListDataSourceMock: ListDataSourceMock<Run, RunListResponse> = new ListDataSourceMock();
+  let store: MockStore;
+  let dispatchSpy: jasmine.Spy<(action: Action) => void>;
 
   beforeEach(async () => {
-    // setup all global fakes and stubs
-    await setupFakesAndStubs();
+    // arrange fakes & stubs
+    // setup experiment fakes
+    fakeArtifacts = await getRandomArtifacts(3);
+    fakeProject = await getRandomProject();
+    fakeExperiment = await getRandomExperiment();
+    fakeRuns = await getRandomRuns(3);
 
     // configure test bed with all dependencies
     TestBed.configureTestingModule({
       declarations: [
         ExperimentDetailsComponent,
+        MockComponent(ExperimentLineageVisualizationComponent),
         MockComponent(ArtifactsListTableComponent),
         MockComponent(RunsListTableComponent),
       ],
       providers: [
-        { provide: ActivatedRoute, useValue: { params: paramMapObservable } },
-        { provide: ArtifactsApiService, useValue: artifactsApiServiceStub },
-        { provide: ExperimentsApiService, useValue: experimentsApiServiceStub },
         { provide: LineageGraphUiService, useValue: lineageGraphServiceStub },
-        { provide: RunsApiService, useValue: runsApiServiceStub },
+        provideMockStore(),
       ],
       imports: [MatButtonModule, MatDialogModule, MatTableModule, MatCardModule],
     }).compileComponents();
+
+    store = TestBed.inject(MockStore);
+
+    store.overrideSelector(selectCurrentExperiment, fakeExperiment);
+    store.overrideSelector(selectCurrentProjectKey, fakeProject.key);
+    store.overrideSelector(selectRunsOfCurrentExperiment, fakeRuns);
+    store.overrideSelector(selectArtifactsByRunKeys, fakeArtifacts);
+    store.overrideSelector(selectIsLoadingRuns, true);
+    store.overrideSelector(selectIsLoadingArtifacts, true);
+
+    dispatchSpy = spyOn(store, 'dispatch');
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ExperimentDetailsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  });
-
-  afterEach(() => {
-    artifactListDataSourceMock.emulate([]);
-    runListDataSourceMock.emulate([]);
   });
 
   it("should create", () => {
@@ -85,31 +91,74 @@ describe("ExperimentDetailsComponent", () => {
   });
 
   describe("ngOnInit", () => {
-    it("should load experiment with projectKey and experimentKey defined in active route", async () => {
+    it("should select experiment from store correctly", async (done) => {
       // arrange + act in beforeEach
 
       // assert
-      expect(component.experiment).toBe(fakeExperiment);
-      expect(experimentsApiServiceStub.getExperiment).toHaveBeenCalledWith(fakeProject.key, fakeExperiment.key);
+      component.experiment$.subscribe((experiment) => {
+        expect(experiment).toBe(fakeExperiment);
+        done();
+      });
     });
 
-    it("should load all runs of specified experiment", async () => {
-      // arrange + act also in beforeEach
-      const fakeRuns: Run[] = await getRandomRuns();
-      setupRunsStub(fakeRuns);
-
-      // assert
-      expect(component.runs).toBe(fakeRuns);
-      expect(runsApiServiceStub.getRunsByExperimentKey).toHaveBeenCalledWith(fakeProject.key, fakeExperiment.key);
-    });
-
-    it("should not load any artifact if there are no runs in the current experiment", async () => {
+    it("should select projectKey from store correctly", async (done) => {
       // arrange + act in beforeEach
 
       // assert
-      expect(artifactsApiServiceStub.getArtifactsByRunKeys).toHaveBeenCalledTimes(0);
+      component.projectKey$.subscribe((projectKey) => {
+        expect(projectKey).toBe(fakeProject.key);
+        done();
+      });
     });
 
+    it("should select runs from store correctly", async (done) => {
+      // arrange + act in beforeEach
+
+      // assert
+      component.runs$.subscribe((runs) => {
+        expect(runs).toBe(fakeRuns);
+        done();
+      });
+    });
+
+    it("should select artifacts from store correctly", async (done) => {
+      // arrange + act in beforeEach
+
+      // assert
+      component.artifacts$.subscribe((artifacts) => {
+        expect(artifacts).toBe(fakeArtifacts);
+        done();
+      });
+    });
+
+    it("should select isLoadingRuns from store correctly", async (done) => {
+      // arrange + act in beforeEach
+
+      // assert
+      component.isLoadingRuns$.subscribe((isLoading) => {
+        expect(isLoading).toBe(true);
+        done();
+      });
+    });
+
+    it("should select isLoadingArtifacts from store correctly", async (done) => {
+      // arrange + act in beforeEach
+
+      // assert
+      component.isLoadingArtifacts$.subscribe((isLoading) => {
+        expect(isLoading).toBe(true);
+        done();
+      });
+    });
+
+    it("should dispatch loadExperimentWithAllDetails action", () => {
+      // ngOnInit will be called in beforeEach while creating the component
+
+      // assert
+      expect(dispatchSpy).toHaveBeenCalledWith(loadExperimentWithAllDetails());
+    });
+
+    /*
     it("should not render experiment lineage if there are no runs in the current experiment", async () => {
       // arrange + act in beforeEach
 
@@ -129,6 +178,7 @@ describe("ExperimentDetailsComponent", () => {
         fakeRuns.map((r) => r.key)
       );
     });
+
 
     it("should map all runs to nodes and edges and call the lineage graph service", async () => {
       // arrange + act also in beforeEach
@@ -202,7 +252,7 @@ describe("ExperimentDetailsComponent", () => {
         expectedEdges,
         component.experimentGraphSvg
       );
-    });
+    });*/
   });
 
   describe("component rendering", () => {
@@ -214,6 +264,7 @@ describe("ExperimentDetailsComponent", () => {
       expect(h1.textContent).toEqual(fakeExperiment.name);
     });
 
+    /*
     it("should contain graph header", async () => {
       // arrange
       const description: HTMLElement = fixture.nativeElement.querySelector("#experiment-graph-header-description");
@@ -230,7 +281,6 @@ describe("ExperimentDetailsComponent", () => {
       // assert
       expect(matCard).toBeTruthy();
     });
-
     it("should contain svg", async () => {
       // arrange
       const svg: HTMLElement = fixture.nativeElement.querySelector("svg");
@@ -238,80 +288,95 @@ describe("ExperimentDetailsComponent", () => {
       // assert
       expect(svg).toBeTruthy();
     });
-
-    it("should contain runs - title and child component", async () => {
-      // arrange
-      const title: HTMLElement = fixture.nativeElement.querySelector("#runs-title");
-      const childComponent: HTMLElement = fixture.nativeElement.querySelector("app-runs-list-table");
-
-      // assert
-      expect(title.textContent).toEqual("Runs");
-      expect(childComponent).toBeTruthy();
-    });
-
-    it("should contain artifacts - title and child component", async () => {
-      // arrange
-      const title: HTMLElement = fixture.nativeElement.querySelector("#artifacts-title");
-      const childComponent: HTMLElement = fixture.nativeElement.querySelector("app-artifacts-list-table");
-
-      // assert
-      expect(title.textContent).toEqual("Artifacts");
-      expect(childComponent).toBeTruthy();
-    });
-  });
-
-  describe("ngOnDestroy", () => {
-    it("should unsubscribe from routeParamsSubscription", async () => {
-      // arrange in beforeEach
-
-      // act
-      component.ngOnDestroy();
-
-      // assert
-      expect(unsubscriptionSpy).toHaveBeenCalled();
-    });
-  });
-
-  function setupRunsStub(runs: Run[]) {
-    // setup runs api
-    runsApiServiceStub.getRunsByExperimentKey
-      .withArgs(fakeProject.key, fakeExperiment.key)
-      .and.returnValue(runListDataSourceMock);
-    runListDataSourceMock.emulate(runs);
-  }
-
-  async function setupFakesAndStubs() {
-    // stub services
-    artifactsApiServiceStub = jasmine.createSpyObj("artifactsApiService", ["getArtifactsByRunKeys"]);
-    experimentsApiServiceStub = jasmine.createSpyObj("experimentsApiService", ["getExperiment"]);
-    lineageGraphServiceStub = jasmine.createSpyObj("lineageGraphService", ["renderLineage"]);
-    runsApiServiceStub = jasmine.createSpyObj("runsApiService", ["getRunsByExperimentKey"]);
-
-    // arrange fakes & stubs
-    // setup experiment fakes
-    fakeProject = await getRandomProject();
-    fakeExperiment = await getRandomExperiment();
-
-    // mock and setup active route
-    paramMapObservable = new Observable<ParamMap>();
-    const paramMapSubscription = new Subscription();
-    unsubscriptionSpy = spyOn(paramMapSubscription, "unsubscribe").and.callThrough();
-    spyOn(paramMapObservable, "subscribe").and.callFake((fn): Subscription => {
-      fn({ projectKey: fakeProject.key, experimentKey: fakeExperiment.key });
-      return paramMapSubscription;
-    });
-
-    // setup experiment api
-    experimentsApiServiceStub.getExperiment.and.returnValue(of(fakeExperiment));
-
-    // setup runs api
-    runsApiServiceStub.getRunsByExperimentKey
-      .withArgs(fakeProject.key, fakeExperiment.key)
-      .and.returnValue(runListDataSourceMock);
-    runListDataSourceMock.emulate([]);
-
-    // setup artifacts api
-    artifactsApiServiceStub.getArtifactsByRunKeys.and.returnValue(artifactListDataSourceMock);
-  }
-});
 */
+
+    describe("child component - app-experiment-lineage-visualization", () => {
+      it("should create", () => {
+        // arrange + act
+        const childComponent: HTMLElement = fixture.nativeElement.querySelector("app-experiment-lineage-visualization");
+
+        // assert
+        expect(childComponent).toBeTruthy();
+      });
+
+      it("should contain title", async () => {
+        // arrange
+        const title: HTMLElement = fixture.nativeElement.querySelector("#graph-title");
+
+        // assert
+        expect(title.textContent).toEqual("Graph");
+      });
+
+      it("should contain correct attributes", async () => {
+        // arrange
+        const experimentLineageVisualizationComponent = ngMocks
+          .find<ExperimentLineageVisualizationComponent>('app-experiment-lineage-visualization')
+          .componentInstance;
+
+        // assert
+        expect(experimentLineageVisualizationComponent.experiment$).toBe(component.experiment$);
+        expect(experimentLineageVisualizationComponent.runs$).toBe(component.runs$);
+      });
+    });
+
+    describe("child component - app-runs-list-table", () => {
+      it("should create", () => {
+        // arrange + act
+        const childComponent: HTMLElement = fixture.nativeElement.querySelector("app-runs-list-table");
+
+        // assert
+        expect(childComponent).toBeTruthy();
+      });
+
+      it("should contain title", async () => {
+        // arrange
+        const title: HTMLElement = fixture.nativeElement.querySelector("#runs-title");
+
+        // assert
+        expect(title.textContent).toEqual("Runs");
+      });
+
+      it("should contain correct attributes", async () => {
+        // arrange
+        const runsListTableComponent = ngMocks
+          .find<RunsListTableComponent>('app-runs-list-table')
+          .componentInstance;
+
+        // assert
+        expect(runsListTableComponent.runs$).toBe(component.runs$);
+        expect(runsListTableComponent.projectKey).toBe(fakeProject.key);
+        expect(runsListTableComponent.isLoading$).toBe(component.isLoadingRuns$);
+      });
+    });
+
+    describe("child component - app-artifacts-list-table", () => {
+      it("should create", () => {
+        // arrange + act
+        const childComponent: HTMLElement = fixture.nativeElement.querySelector("app-artifacts-list-table");
+
+        // assert
+        expect(childComponent).toBeTruthy();
+      });
+
+      it("should contain title", async () => {
+        // arrange
+        const title: HTMLElement = fixture.nativeElement.querySelector("#artifacts-title");
+
+        // assert
+        expect(title.textContent).toEqual("Artifacts");
+      });
+
+      it("should contain correct attributes", async () => {
+        // arrange
+        const artifactsListTableComponent = ngMocks
+          .find<ArtifactsListTableComponent>('app-artifacts-list-table')
+          .componentInstance;
+
+        // assert
+        expect(artifactsListTableComponent.artifacts$).toBe(component.artifacts$);
+        expect(artifactsListTableComponent.projectKey).toBe(fakeProject.key);
+        expect(artifactsListTableComponent.isLoading$).toBe(component.isLoadingArtifacts$);
+      });
+    });
+  });
+});
