bb5f0281b315170a51fa983ae99bf4f77d23218a
Farruch Kouliev <farruch.kouliev@gmail.com>
2021-08-01 12:12:17 +0200

    MOD-117 added unit tests for artifacts-list.component.spec.ts

--- web-ui/src/app/state/artifact/artifact.selectors.ts
+++ web-ui/src/app/state/artifact/artifact.selectors.ts
@@ -1,6 +1,22 @@
 import { AppState } from "../app.state";
+import { createFeatureSelector, createSelector } from "@ngrx/store";
+import { ArtifactState } from "@mlaide/state/artifact/artifact.state";
 
-export const selectArtifacts = (state: AppState) => state.artifacts.items;
-export const selectArtifactsByRunKeys = (state: AppState) => state.artifacts.artifactsByRunKeys;
-export const selectIsLoadingArtifacts = (state: AppState) => state.artifacts.isLoading;
-export const selectModels = (state: AppState) => state.artifacts.models;
+export const artifactState = createFeatureSelector<AppState, ArtifactState>("artifacts");
+
+export const selectArtifacts = createSelector(
+  artifactState,
+  (artifactState) => artifactState.items
+);
+export const selectIsLoadingArtifacts = createSelector(
+  artifactState,
+  (artifactState) => artifactState.isLoading
+);
+export const selectArtifactsByRunKeys = createSelector(
+  artifactState,
+  (artifactState) => artifactState.artifactsByRunKeys
+);
+export const selectModels = createSelector(
+  artifactState,
+  (artifactState) => artifactState.models
+);
