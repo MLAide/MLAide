43d25f2e29bbd44c45383dfa1c8e170fdf5b1a27
Farruch Kouliev <farruch.kouliev@gmail.com>
2022-09-02 21:31:39 +0200

    MOD-203 fixed unittest in backend

--- webserver/src/test/java/com/mlaide/webserver/faker/ArtifactFaker.java
+++ webserver/src/test/java/com/mlaide/webserver/faker/ArtifactFaker.java
@@ -3,13 +3,20 @@ package com.mlaide.webserver.faker;
 import com.github.javafaker.Faker;
 import com.mlaide.webserver.model.Artifact;
 import com.mlaide.webserver.repository.entity.ArtifactEntity;
+import com.mlaide.webserver.repository.entity.RunRefEntity;
 import org.bson.types.ObjectId;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ArtifactFaker {
     private static final Faker faker = new Faker();
 
     public static ArtifactEntity newArtifactEntity() {
         var artifact = new ArtifactEntity();
+        var runRefFaker = RunRefFaker.newRunRefEntity();
+        var runList = new ArrayList<RunRefEntity>();
+        runList.add(runRefFaker);
         artifact.setCreatedAt(FakerUtils.pastDate());
         artifact.setCreatedBy(FakerUtils.newUserRef());
         artifact.setId(ObjectId.get());
@@ -17,6 +24,7 @@ public class ArtifactFaker {
         artifact.setProjectKey(ProjectFaker.validProjectKey());
         artifact.setType(faker.animal().name());
         artifact.setVersion(faker.random().nextInt(Integer.MAX_VALUE));
+        artifact.setRuns(runList);
 
         return artifact;
     }
